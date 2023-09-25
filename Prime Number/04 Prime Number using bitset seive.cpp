caa9b355872b5faf9223fd8b6a9312f65c61bd13
MD Naimur Rahman <69746759+MNR-Tushar@users.noreply.github.com>
2023-09-26 02:38:14 +0600

    Add files via upload

---
+++ Prime Number/04 Prime Number using bitset seive.cpp
@@ -0,0 +1,59 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+#define     ll              long long int
+#define     all(v)          v.begin(),v.end()
+#define     Max             1000000000000000014
+#define     Min             -1000000000000000014
+#define     pb              push_back
+#define     PI              3.1415926535897
+#define endl '\n'
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+const int MX = 1e6+123;
+bitset<MX> is_prime;
+vector<int> prime;
+
+void primeGen ( int n )
+{
+
+    for ( int i = 3; i <= n; i += 2 )
+        is_prime[i] = 1;
+
+    int sq = sqrt ( n );
+    for ( int i = 3; i <= sq; i += 2 )
+    {
+        if ( is_prime[i] == 1 )
+        {
+            for ( int j = i*i; j <= n; j += ( i + i ) )
+            {
+                is_prime[j] = 0;
+            }
+        }
+    }
+
+    is_prime[2] = 1;
+    prime.push_back (2);
+
+    for ( int i = 3; i <= n; i += 2 )
+    {
+        if ( is_prime[i] == 1 )
+            prime.push_back ( i );
+    }
+}
+
+int main()
+{
+    optimize();
+
+    primeGen(2000);
+
+    for(auto u:prime)
+        cout << u <<" ";
+    cout << endl;
+
+
+
+    return 0;
+}
+
